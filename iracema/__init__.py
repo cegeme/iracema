07db1441c88ad7ae409b926a1267f9339b650721
Tairone Nunes Magalhães <taironemagalhaes@gmail.com>
2019-06-15 14:52:47 -0300

    Automatically import modules/subpackages

--- iracema/__init__.py
+++ iracema/__init__.py
@@ -3,5 +3,13 @@ iracema is a python package aimed at the extraction of expressive music
 information from audio signals
 """
 
-from .audio import *
-from .segment import *
+from .audio import Audio
+from .timeseries import TimeSeries
+from .segment import Segment, SegmentList
+
+import iracema.features
+import iracema.harmonics
+import iracema.pitch
+import iracema.plot
+import iracema.segmentation
+import iracema.spectral
