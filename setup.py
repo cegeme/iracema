338aa5c7f7bf993b5dad1e9d94abf3934b7255ce
Tairone Nunes Magalhaes <taironemagalhaes@gmail.com>
2020-05-17 19:28:11 -0300

    Fixes informations to upload project to PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='iracema',
-    version='0.0.1',
+    version='0.1.1dev0',
+    url='http://github.com/cegeme/iracema',
+    author='Tairone Magalhães',
+    author_email='taironemagalhaes@gmail.com',
     description='Audio Content Analysis for Research on Musical Expressiveness and Individuality',
-    author='Tairone Magalhães <taironemagalhaes@gmail.com>',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     install_requires=[
         'numpy>=1.18.4',
         'scipy>=1.4.1',
@@ -12,10 +19,14 @@ setup(
         'audioread>=2.1.8',
         'matplotlib==3.2.1'],
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
     ],
-    packages=find_packages())
+    packages=find_packages(),
+    )
