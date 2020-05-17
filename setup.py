00bbafe8073f720c82ec6821e740c898c91041eb
Tairone Nunes Magalhaes <taironemagalhaes@gmail.com>
2020-05-17 17:54:37 -0300

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version='0.0.1',
     description='Audio Content Analysis for Research on Musical Expressiveness and Individuality',
     author='Tairone Magalhães <taironemagalhaes@gmail.com>',
-    install_requires=['numpy>=1.9.1', 'scipy>=0.14'],
+    install_requires=[
+        'numpy>=1.18.4',
+        'scipy>=1.4.1',
+        'sounddevice>=0.3.12',
+        'audioread>=2.1.8',
+        'matplotlib==3.2.1'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
