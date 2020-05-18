0a55e1c21205bae10481a11538533744246eaaed
Tairone Magalhães <taironemagalhaes@gmail.com>
2020-05-18 00:43:23 -0300

    Bump to version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='iracema',
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/cegeme/iracema',
     author='Tairone Magalhães',
     author_email='taironemagalhaes@gmail.com',
