a2ca17c5a8a5e0ed7fed309ba0840f7fc96458fc
Tairone Nunes Magalhaes <taironemagalhaes@gmail.com>
2020-11-02 11:51:36 -0300

    Assign version to variable

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ author = u'Tairone N. Magalhães'
 # built documents.
 #
 # The short X.Y version.
-version = u'0' 
+version = u'0.1.4' 
  # The full version, including alpha/beta/rc tags.
 release = version
 
