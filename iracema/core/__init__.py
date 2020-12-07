69e486396c978afa086bc4b7f4a11e6c15644528
Tairone Magalhães <taironemagalhaes@gmail.com>
2020-12-07 00:21:59 -0300

    Add docstring

--- iracema/core/__init__.py
+++ iracema/core/__init__.py
@@ -1,4 +1,7 @@
-# from .audio import Audio
-# from .timeseries import TimeSeries
-# from .point import Point, PointList
-# from .segment import Segment, SegmentList
+"""
+Contains modules that implement the core classes of the project. To make these
+classes easily available for users, they are all already imported into the
+namespace `iracema`. For instance, the class ``Audio`` is available in the
+module ``iracema.core.audio``, but it can be accessed using the shorter name
+``iracema.Audio``.
+"""
