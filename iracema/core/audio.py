4950d6265b6c6fb2d356c619b3b33272860b4801
Tairone Magalhães <taironemagalhaes@gmail.com>
2020-12-07 00:17:28 -0300

    Refactor class and improve docstrings

--- iracema/core/audio.py
+++ iracema/core/audio.py
@@ -1,92 +1,117 @@
 """
-Definition of the class Audio.
+This module contains the implementation of the class ``Audio``.
 """
 from iracema.core.timeseries import TimeSeries
-from iracema.io.audiofile import read
+from iracema.io.audiofile import read as _read
 from iracema.io import player
 
 
 class Audio(TimeSeries):
     """
-    Stores audio data, which can be loaded from a file or an array.
+    Class for storing and manipulating audio data.
 
-    Parameters
-    ----------
-    file_location : str, optional
-        Location from where the file will be loaded. The string might
-        contain a path pointing to a local file or an http URL referencing
-        a remote file.
-    data : np.array
-        Data vector containing the audio data to be loaded.
-    fs : int, optional
-        Sampling frequency of the data.
-    caption : str, optional
-        caption for the audio file loaded (optional).
-
-    Examples
-    --------
-    There are two different ways to initialize an Audio object: from
-    audio files or from NumPy arrays.
-
-    To initialize it using an audio file, you just need to pass the location
-    from which the file must be loaded:
-
-    >>> a = ir.Audio('03 - Clarinet - Fast Excerpt.wav')
-
-    Alternatively the location can be specified trough an http URL:
-
-    >>> url = (
-    ... 'https://raw.githubusercontent.com/cegeme/iracema-audio/master/03 - '
-    ... 'Clarinet - Fast Excerpt.wav'
-    ... )
-    >>> a = ir.Audio(url)
-
-    To initialize it using a NumPy array, two arguments are necessary: the
-    ``data`` array and the sampling frequency ``fs``:
-
-    >>> a = ir.Audio(clarinet_data, 44100)  # doctest: +SKIP
-
-    There is also the optional parameter ``caption`` which is a
-    textual description used for plotting and displaying reports
-    about the audio file. In case you load the audio from a file
-    and don't specify a caption, the filename will be assigned to
-    it.
+    .. Hint:: This class is also available at the main package level as
+        ``iracema.Audio``.
     """
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, fs, data, start_time=0., caption=None):
         """
-        Constructor for Audio class.
+        Example
+        -------
+        There are two ways to instantiate an ``Audio`` object: from audio files
+        or from `NumPy`_ arrays. To initialize it using an audio file, you must
+        use the method ``Audio.load()``, specifying the location from which the
+        file must be loaded:
+
+        >>> ir.Audio.load('02 - Clarinet - Long Notes.wav')
+        Audio: (1, 1785244), fs=44100.0, unit=amplitude, label=waveform
+
+        Alternatively the location can be specified trough an http URL:
+
+        >>> url = (
+        ... 'https://raw.githubusercontent.com/cegeme/iracema-audio/master/03 - '
+        ... 'Clarinet - Fast Excerpt.wav'
+        ... )
+        >>> ir.Audio.load(url)
+        Audio: (1, 390060), fs=44100.0, unit=amplitude, label=waveform
+
+        To initialize the object using a NumPy array, you can use the
+        initializer of the class ``Audio()``. In this case, two arguments
+        are necessary: the sampling frequency ``fs`` of the data and the
+        ``data`` array.
+
+        >>> ir.Audio(44100, clarinet_data)
+        Audio: (1, 390060), fs=44100.0, unit=amplitude, label=waveform
+
+        .. _NumPy:
+           https://numpy.org/doc/
+
+        Args
+        ----
+        fs : float
+            Sampling frequency for the data.
+        data : numpy array
+            Data array containing the audio samples.
+        start_time : float, optional
+            The time in seconds the time series start, relative to the original
+            time reference.
+        unit : str, optional
+            Unit name for plotting the data of the time series.
+        caption : str, optional
+            Text caption for the time series.
+        data : np.array, optional
+            Data vector containing the audio data to be loaded.
+        fs : int, optional
+            Sampling frequency of the data.
+        caption : str, optional
+            Textual description used for plotting and displaying reports about
+            the audio excerpt.
         """
-        self.unit = 'amplitude'
+        unit = 'amplitude'
         self.label = 'waveform'
+        self.filename, self.caption = None, caption
 
-        nargs = len(args)
-        caption = kwargs.get('caption', None)
+        super(Audio, self).__init__(
+            fs,
+            data=data,
+            unit=unit,
+            start_time=start_time,
+            caption=caption)
 
-        # one argument: file name
-        if nargs == 1:
-            filename = args[0]
-            data, fs, basename = read(filename)
-            self.filename = basename
-            self.caption = caption or self.filename
+    @classmethod
+    def load(cls, file_location, caption=None):
+        """
+        Load an audio file into an ``Audio`` object.
 
-        # two arguments: an array and a sampling frequency
-        elif nargs == 2:
-            data, fs = args[0], args[1]
-            self.filename, self.caption = None, caption
+        Parameters
+        ----------
+        file_location : str
+            Location from which the file will be loaded. The string might
+            contain a path pointing to a local file or an http URL referencing
+            a remote file
+        caption : str, optional
+            Caption for the audio file loaded (optional). If this argument is
+            not provided, the base name of the loaded file will be used.
 
-        else:
-            raise (TypeError(
-                'invalid number of positional arguments: should be '
-                '1 or 2'))
-
-        super(Audio, self).__init__(fs, data=data, unit=self.unit)
+        Return
+        ------
+        audio : Audio
+            An object of the class ``Audio``, containing the data loaded from
+            the specified location.
+        """
+        data, fs, base_name = _read(file_location)
+        caption = caption or base_name
+        audio = cls(fs, data, caption=caption)
+        audio.filename = base_name
+        return audio
 
     def plot(self, linewidth=0.1, alpha=0.9, **kwargs):
         """
-        Plot the time series using matplotlib.
-        Line width and alpha values can be set as optional parameters.
+        Plot the time series. The ``linewidth`` and ``alpha`` (opacity) values
+        can be set using keyword arguments.
         """
-        return super(Audio, self).plot(linewidth=linewidth, alpha=alpha, **kwargs)
+        return super(Audio, self).plot(
+            linewidth=linewidth, alpha=alpha, **kwargs)
 
     def play(self):
         """
@@ -104,7 +129,8 @@ class Audio(TimeSeries):
         """
         Play segment from Audio obejct.
         """
-        return player.play_interval_seconds(self, segment.start_time, segment.end_time)
+        return player.play_interval_seconds(self, segment.start_time,
+                                            segment.end_time)
 
     def stop(self):
         """
