a7533abde46bfbae9e4b4dea846d46e2073a1524
Tairone Magalhães <taironemagalhaes@gmail.com>
2020-12-07 00:23:41 -0300

    Update test to use Audio.load

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,23 +5,23 @@ from iracema import Audio
 
 @pytest.fixture(scope="module")
 def audio00():
-    return Audio('audio/iracema-audio/00 - Flute - Iracema.wav')
+    return Audio.load('audio/iracema-audio/00 - Flute - Iracema.wav')
 
 @pytest.fixture(scope="module")
 def audio01():
-    return Audio('audio/iracema-audio/01 - Guitar - Stil Got The Blues.wav')
+    return Audio.load('audio/iracema-audio/01 - Guitar - Stil Got The Blues.wav')
 
 @pytest.fixture(scope="module")
 def audio02():
-    return Audio('audio/iracema-audio/02 - Clarinet - Long Notes.wav')
+    return Audio.load('audio/iracema-audio/02 - Clarinet - Long Notes.wav')
 
 @pytest.fixture(scope="module")
 def audio03():
-    return Audio('audio/iracema-audio/03 - Clarinet - Fast Excerpt.wav')
+    return Audio.load('audio/iracema-audio/03 - Clarinet - Fast Excerpt.wav')
 
 @pytest.fixture(scope="module")
 def audio_url():
-    return Audio('https://raw.githubusercontent.com/cegeme/iracema-audio/master/03 - Clarinet - Fast Excerpt.wav')
+    return Audio.load('https://raw.githubusercontent.com/cegeme/iracema-audio/master/03 - Clarinet - Fast Excerpt.wav')
 
 def pytest_addoption(parser):
     parser.addoption(
