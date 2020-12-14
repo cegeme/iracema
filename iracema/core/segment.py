18313616a38c2158515f192e045fece5f43d2c64
Tairone Magalhães <taironemagalhaes@gmail.com>
2020-12-13 23:10:15 -0300

    Refactor segment module

--- iracema/core/segment.py
+++ iracema/core/segment.py
@@ -2,7 +2,10 @@
 This module contain classes used to manipulate segments and slice TimeSeries
 objects using them.
 """
+from collections.abs import MutableSequence
+from decimal import Decimal
 
+from iracema.core.point import Point, PointList
 from iracema.util import conversion
 
 
@@ -14,7 +17,8 @@ class Segment:
     .. Hint:: This class is also available at the main package level as
         ``iracema.Segment``.
     """
-    def __init__(self, time_series, start, end, limits_unit='sample_index'):
+
+    def __init__(self, start, end):
         """
         The `start` and `end` arguments can be specified in terms of sample
         index or time (in seconds). The argument `limits_unit` must be set
@@ -22,93 +26,92 @@ class Segment:
 
         Args
         ----
-        time_series : TimeSeries
-            Original time series related to the segment.
-        start : int or float
+        start : Point
             Index (or sample number) corresponding to the start of the segment
             in the time-series from which it derived. Alternatively, this
             value can be specified in seconds.
         end : int or float
             Index of the ending sample for the segment. Alternatively, this
             value can be specified in seconds.
-        limits_unit : ("sample_index", "seconds")
-            If 'sample_index' is passed (default), the arguments `start` and
-            `end` must be integers corresponding to sample indexes whitin
-            `time_series`. Else, if 'seconds' is passed, both arguments must
-            correspond to the time of these limits, in seconds.
         """
-        if limits_unit not in ('sample_index', 'seconds'):
-            raise ValueError("invalid value for `limits_unit` argument: must"
-                             " be 'sample_index' or 'seconds'")
-
         if end is not None and start > end:
             raise ValueError("end must be > start")
 
-        self.fs = time_series.fs
-        self.time_offset = time_series.start_time
+        self.start = Point(start)
+        self.end = Point(end)
 
-        if limits_unit == 'sample_index':
-            if type(start) != int or type(end) != int:
-                raise ValueError("`start` and `end` must be of type int when"
-                                 "`limits_unit`=='sample_index'")
-            self.start = start
-            self.end = end
-
-        elif limits_unit == 'seconds':
-            self.start = conversion.seconds_to_sample_index(start, self.fs)
-            self.end = conversion.seconds_to_sample_index(end, self.fs)
+    def nsamples(self, time_series):
+        return (self.end.map_index(time_series) -
+                self.start.map_index(time_series))
 
     @property
-    def nsamples(self):
+    def duration(self):
         return self.end - self.start
 
     @property
     def start_time(self):
-        return conversion.sample_index_to_seconds(self.start, self.fs,
-                                                  self.time_offset)
+        return self.start
 
     @property
     def end_time(self):
-        return conversion.sample_index_to_seconds(self.end, self.fs,
-                                                  self.time_offset)
+        return self.end
 
     def generate_slice(self, time_series):
         """
         Generate a python slice with the sample indexes that correspond to the
         current segment in `time_series`
         """
-        new_fs = time_series.fs
-        new_time_offset = time_series.start_time
-
-        if self.fs == time_series.fs:
-            return slice(self.start, self.end)
-        slice_start = conversion.map_sample_index(
-            self.start, self.fs, self.time_offset, new_fs, new_time_offset)
-        slice_end = conversion.map_sample_index(
-            self.end, self.fs, self.time_offset, new_fs, new_time_offset)
-
+        slice_start = self.start.map_index(time_series)
+        slice_end = self.end.map_index(time_series)
         return slice(slice_start, slice_end)
 
     def __repr__(self):
         "Overload the representation for the class"
         class_name = self.__class__.__name__
-        description = "(start: {}, end: {}), fs: {}".format(
-            self.start, self.end, self.fs)
+        description = "(start: {}, end: {})".format(self.start, self.end)
         return '{}: {}'.format(class_name, description)
 
 
-class SegmentList(list):
+class SegmentList(MutableSequence):
     """
     List of segments.
 
     .. Hint:: This class is also available at the main package level as
         ``iracema.SegmentList``.
     """
-    def __init__(self, segment_list):
+
+    def __init__(self, segments=None):
+        super(SegmentList, self).__init__()
+        if (segments is not None):
+            self._segments = list(segments)
+        else:
+            self._segments = list()
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return SegmentList(self._segments[index])
+        return self._segments[index]
+
+    def __setitem__(self, index, item):
+        if not isinstance(item, Segment):
+            raise ValueError(
+                "The list contains an item that is not a ``Segment``")
+        self._segments[index] = item
+
+    def __delitem__(self, index):
+        self._segments.__delitem__(index)
+
+    def __len__(self):
+        return len(self._segments)
+
+    def insert(self, index, item):
+        if not isinstance(item, Segment):
+            raise ValueError("The insert item is not a ``Segment``")
+        return self._segments.insert(index, item)
+
+    def map_indexes(self, time_series):
         """
-        Args
-        ----
-        point_list : list[Segment]
-            A list of segments.
+        Return an array with the indexes of ``time_series`` that correspond to
+        the segments in the list.
         """
-        super(SegmentList, self).__init__(segment_list)
+        return [segment.map_index(time_series) for segment in self]
