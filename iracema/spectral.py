24e856020786714d1e2e6a4ef6d98e5e68fdd730
Tairone Nunes Magalhaes <taironemagalhaes@gmail.com>
2021-01-11 19:30:49 -0300

    Replace method fft by class STFT

--- iracema/spectral.py
+++ iracema/spectral.py
@@ -1,13 +1,53 @@
 """
 Extraction of spectral information.
 """
+from decimal import Decimal
+
 import numpy as np
-from iracema.aggregation import sliding_window
+from deprecated.sphinx import deprecated
+from librosa.filters import mel
+from librosa.core.convert import mel_frequencies
+
+from iracema.util.windowing import apply_sliding_window
+import iracema.core.timeseries
 
 
-def fft(time_series, window_size, hop_size, fft_len=4096):
+class STFT(iracema.core.timeseries.TimeSeries):
+    "Compute the Short-Time Fourier Transform for the ``time_series``."
+    def __init__(self, time_series, window_size, hop_size, fft_len=4096):
     """
-    Calculate the Fast Fourier Transform for the ``time_series``
+        Args
+        ----
+        time_series : TimeSeries
+            Time series for applying the STFT.
+        window_size : int
+        hop_size : int
+        fftlen : int
+            Length of the FFT. The signal will be zero-padded if ``fftlen`` >
+            ``rolling_window.window_size``.
+        """
+        def calculate(x):
+            return np.fft.rfft(x, n=fft_len)
+
+        stft_data = apply_sliding_window(
+            time_series.data, window_size, hop_size, calculate, window_name='hann')
+
+        new_fs = Decimal(time_series.fs) / Decimal(hop_size)
+
+        super(STFT, self).__init__(
+            new_fs, data=stft_data, start_time=time_series.start_time, caption=time_series.caption)
+
+        self.max_frequency = time_series.nyquist
+        self.frequencies = np.fft.rfftfreq(fft_len, 1. / time_series.fs)
+
+        self.label = 'STFT'
+        self.unit = ''
+
+    def magnitude(self, power=2.):
+        return np.abs(self.data) ** power
+
+    def phase(self):
+        return np.angle(self.data)
 
     Args
     ----
@@ -30,7 +70,8 @@ def fft(time_series, window_size, hop_size, fft_len=4096):
     fft_time_series.frequencies = np.linspace(
         0, fft_time_series.max_frequency, fft_len, dtype=np.float_)
 
-    fft_time_series.label = 'FFT'
-    fft_time_series.unit = 'Magnitude'
 
-    return fft_time_series
+@deprecated(version='0.2.0', reason='Deprecated method. Use `STFT` instead.')
+def fft(*args, **kwargs):
+    "Deprecated FFT method."
+    return STFT(*args, **kwargs)
