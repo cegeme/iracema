22cb2b967bff043fbd8fa39924bd29f8cfb5d367
Tairone Nunes Magalhaes <taironemagalhaes@gmail.com>
2021-01-15 12:46:38 -0300

    Fix test_segment_list

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -17,9 +17,11 @@ def test_segment_nsamples(audio01, audio02):
 
 
 def test_segment_list(audio01):
-    s1 = ir.Segment(1, 2)
-    s2 = ir.Segment(3, 4)
-    s3 = ir.Segment(5, 6)
-    seg_list = ir.SegmentList(segments=[s1, s2])
-    seg_list.append(s3)
-    assert seg_list == [s1, s2, s3]
+    s0 = ir.Segment(1, 2)
+    s1 = ir.Segment(3, 4)
+    s2 = ir.Segment(5, 6)
+    seg_list = ir.SegmentList(segments=[s0, s1])
+    seg_list.append(s2)
+    assert seg_list[0] == s0
+    assert seg_list[1] == s1
+    assert seg_list[2] == s2
